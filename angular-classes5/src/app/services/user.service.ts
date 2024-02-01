ec30f6540b557df12c80338e61eca9cfbf98bd06
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-02-01 11:47:33 -0300

    logout and redirections

--- angular-classes5/src/app/services/user.service.ts
+++ angular-classes5/src/app/services/user.service.ts
@@ -5,6 +5,7 @@ import {
   TUserReturn,
 } from '../interfaces/user.interface';
 import { UserRequest } from '../api/user.request';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ import { UserRequest } from '../api/user.request';
 export class UserService {
   readonly userSignal = signal<TUserReturn | null>(null);
 
-  constructor(private userRequest: UserRequest) {
+  constructor(private userRequest: UserRequest, private router: Router) {
     this.userRequest.getUser()?.subscribe((data) => {
       this.userSignal.set(data);
     });
@@ -29,6 +30,14 @@ export class UserService {
       this.userSignal.set(data.user);
       localStorage.setItem('@TOKEN', JSON.stringify(data.accessToken));
       localStorage.setItem('@USERID', JSON.stringify(data.user.id));
+      this.router.navigateByUrl("/dashboard");
     });
+  }
+
+  logout() {
+    this.userSignal.set(null);
+    localStorage.removeItem('@TOKEN');
+    localStorage.removeItem('@USERID');
+    this.router.navigateByUrl("/");
   }
 }
