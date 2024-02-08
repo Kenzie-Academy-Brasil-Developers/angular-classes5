2fcff8fe2221499e7db6236be57dcb1109473c54
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-02-08 15:55:54 -0300

    removing code

--- angular-classes5/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ angular-classes5/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
-import { UserService } from '../../services/user.service';
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
-export class DashboardPageComponent {
-  constructor(private userService: UserService, private router: Router) {
-    if(!this.user) this.router.navigateByUrl("/");
-  }
-
-  get user(){
-    return this.userService.getUser();
-  }
-
-  handleLogout(){
-    this.userService.logout();
-  }
-}
+export class DashboardPageComponent { }
--- angular-classes5/src/app/pages/home-page/home-page.component.ts
+++ angular-classes5/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { LoginFormComponent } from '../../components/forms/login-form/login-form.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [LoginFormComponent, RouterLink],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
