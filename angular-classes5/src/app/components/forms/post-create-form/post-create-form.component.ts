126de3a06a22bb2f365716780b9ca23b6381818e
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-02-15 17:58:50 -0300

    implementing post create

--- angular-classes5/src/app/components/forms/post-create-form/post-create-form.component.ts
+++ angular-classes5/src/app/components/forms/post-create-form/post-create-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { PostService } from '../../../services/post.service';
+import { TCreatePostFormData } from '../../../interfaces/post.interface';
 
 @Component({
   selector: 'app-post-create-form',
@@ -9,9 +11,17 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './post-create-form.component.scss'
 })
 export class PostCreateFormComponent {
+  constructor(private postService: PostService) {}
+
   postCreateForm = new FormGroup({
-    category: new FormControl(null),
-    title: new FormControl(null),
-    content: new FormControl(null),
+    category: new FormControl<string | null>(null),
+    title: new FormControl<string | null>(null),
+    content: new FormControl<string | null>(null),
   });
+
+  submit(){
+    const data = this.postCreateForm.value as TCreatePostFormData;
+    this.postService.create(data);
+    this.postCreateForm.reset();
+  }
 }
--- angular-classes5/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ angular-classes5/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { PrivateRouteComponent } from '../../components/private-route/private-route.component';
+import { PostCreateFormComponent } from '../../components/forms/post-create-form/post-create-form.component';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [PrivateRouteComponent],
+  imports: [PrivateRouteComponent, PostCreateFormComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
