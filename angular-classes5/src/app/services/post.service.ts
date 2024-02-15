7976368f60b20b6ae984610e38a3cbe4855fe983
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-02-15 16:46:45 -0300

    post service part 2

--- angular-classes5/src/app/services/post.service.ts
+++ angular-classes5/src/app/services/post.service.ts
@@ -1,26 +1,50 @@
 import { Injectable, signal } from '@angular/core';
-import { IPost, TCreatePostData } from '../interfaces/post.interface';
+import {
+  IPost,
+  TCreatePostData,
+  TUpdatePostData,
+} from '../interfaces/post.interface';
 import { PostRequest } from '../api/post.request';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
   readonly postListSignal = signal<IPost[]>([]);
 
-  constructor(private postRequest: PostRequest) { 
-    this.postRequest.getPosts().subscribe(data => {
+  constructor(private postRequest: PostRequest) {
+    this.postRequest.getPosts().subscribe((data) => {
       this.postListSignal.set(data);
-    })
+    });
   }
 
-  getPosts(){
+  getPosts() {
     return this.postListSignal();
   }
 
-  create(formData: TCreatePostData){
-    this.postRequest.create(formData)?.subscribe(data => {
+  create(formData: TCreatePostData) {
+    this.postRequest.create(formData)?.subscribe((data) => {
       this.postListSignal.update((postList) => [...postList, data]);
+    });
+  }
+
+  update(id: number, formData: TUpdatePostData) {
+    this.postRequest.update(id, formData)?.subscribe((data) => {
+      this.postListSignal.update((postList) =>
+        postList.map((post) => {
+          if (post.id === id) {
+            return data;
+          } else {
+            return post;
+          }
+        })
+      );
+    });
+  }
+
+  delete(id: number) {
+    this.postRequest.delete(id)?.subscribe(() => {
+      this.postListSignal.update((postList) => postList.filter(post => post.id !== id));
     })
   }
 }
