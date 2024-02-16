f5ce42c73f52a9fa6b203f69286b921102c17866
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-02-16 15:07:00 -0300

    feat: implementing post read

--- angular-classes5/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ angular-classes5/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -2,22 +2,31 @@ import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { PrivateRouteComponent } from '../../components/private-route/private-route.component';
 import { PostCreateFormComponent } from '../../components/forms/post-create-form/post-create-form.component';
+import { PostService } from '../../services/post.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [PrivateRouteComponent, PostCreateFormComponent],
+  imports: [PrivateRouteComponent, PostCreateFormComponent, CommonModule],
   templateUrl: './dashboard-page.component.html',
-  styleUrl: './dashboard-page.component.scss'
+  styleUrl: './dashboard-page.component.scss',
 })
 export class DashboardPageComponent {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private postService: PostService
+  ) {}
 
-  get user(){
+  get user() {
     return this.userService.getUser();
   }
 
-  handleLogout(){
+  get postList() {
+    return this.postService.getPosts();
+  }
+
+  handleLogout() {
     this.userService.logout();
   }
 }
